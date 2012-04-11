6392c0f05a16adf43446e9b968dfaa0a13f659d9
Justin Bakse <justin@tsfim.com>
2012-04-10 22:59:33 -0400

    added flood to test example

--- examples/test/js/ncs_test.coffee
+++ examples/test/js/ncs_test.coffee
@@ -1,4 +1,6 @@
 #start
+count = 0
+
 $ ()->
 	console.log "start"
 	ncs.connect location.host, "ncs_test", ()->
@@ -8,14 +10,35 @@ $ ()->
 	ncs.onreceive (_key, _value)->
 		if _key == 'echo'
 			time = Date.now() - _value
-			screenLog """received echo(#{_value}) in #{time}ms"""
+			screenLog """received echo(#{_value}) in #{time}ms # #{count++}"""
+
+		if _key == 'flood'
+			screenLog "got flood", _value.fps, _value.duration
+			i = every 1000/(_value.fps), ()->
+				screenLog 'sending echo: testing'
+				ncs.send 'echo', Date.now()
+			after _value.duration, ()->
+				screenLog 'DONE DONE DONE'
+				screenLog 'DONE DONE DONE'
+				clearInterval i
 
 	$("#send").click (event)->
 		screenLog 'sending echo: testing'
 		ncs.send 'echo', Date.now()
+
+	$("#flood").click (event)->
+		if !confirm "Really? Truly?" then return
+		screenLog 'sending echo: flooding'
+		ncs.send 'flood', {fps: 50, duration: 5000}
+
 
 #util
 screenLog = (_value)->
 	$("#console").append $ """<tr><td>#{_value.toString()}</td></tr>"""
 
 
+every = (_ms, _callback) -> 
+	return setInterval _callback, _ms
+
+after = (_ms, _callback) -> 
+	return setTimeout _callback, _ms
--- examples/test/js/ncs_test.js
+++ examples/test/js/ncs_test.js
@@ -1,5 +1,7 @@
 (function() {
-  var screenLog;
+  var after, count, every, screenLog;
+
+  count = 0;
 
   $(function() {
     console.log("start");
@@ -8,20 +10,48 @@
       return ncs.send('echo', Date.now());
     });
     ncs.onreceive(function(_key, _value) {
-      var time;
+      var i, time;
       if (_key === 'echo') {
         time = Date.now() - _value;
-        return screenLog("received echo(" + _value + ") in " + time + "ms");
+        screenLog("received echo(" + _value + ") in " + time + "ms # " + (count++));
+      }
+      if (_key === 'flood') {
+        screenLog("got flood", _value.fps, _value.duration);
+        i = every(1000 / _value.fps, function() {
+          screenLog('sending echo: testing');
+          return ncs.send('echo', Date.now());
+        });
+        return after(_value.duration, function() {
+          screenLog('DONE DONE DONE');
+          screenLog('DONE DONE DONE');
+          return clearInterval(i);
+        });
       }
     });
-    return $("#send").click(function(event) {
+    $("#send").click(function(event) {
       screenLog('sending echo: testing');
       return ncs.send('echo', Date.now());
+    });
+    return $("#flood").click(function(event) {
+      if (!confirm("Really? Truly?")) return;
+      screenLog('sending echo: flooding');
+      return ncs.send('flood', {
+        fps: 50,
+        duration: 5000
+      });
     });
   });
 
   screenLog = function(_value) {
     return $("#console").append($("<tr><td>" + (_value.toString()) + "</td></tr>"));
+  };
+
+  every = function(_ms, _callback) {
+    return setInterval(_callback, _ms);
+  };
+
+  after = function(_ms, _callback) {
+    return setTimeout(_callback, _ms);
   };
 
 }).call(this);
